e8c4dc809cc74d69d6800c665af0a537aa063205
Muzietto <marco_faustinelli@yahoo.com>
2015-10-14 15:24:45 +0200

    first iterator test

--- es6/iterators_es6_test.js
+++ es6/iterators_es6_test.js
@@ -10,7 +10,19 @@
 var expect = chai.expect;
 mocha.setup('bdd');
 
-describe('xxx', function() {
-  it('yyy', function() {
+function expect_from_iterator(iter, value, done) {
+  var data = iter.next();
+  expect(data.value).to.be.eql(value);  
+  expect(data.done).to.be.eql(done);  
+}
+
+describe('an array iterator', function() {
+  it('consumes the whole array', function() {
+    var arr = ['a','b','c'];
+    var arr_iterator = arr[Symbol.iterator]();
+    expect_from_iterator(arr_iterator, 'a', false);
+    expect_from_iterator(arr_iterator, 'b', false);
+    expect_from_iterator(arr_iterator, 'c', false);
+    expect_from_iterator(arr_iterator, undefined, true);
   });
 });
