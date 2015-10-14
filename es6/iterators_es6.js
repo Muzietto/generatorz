1bcad1a634fa08cdb3200ed7441b6f006b13939a
Muzietto <marco_faustinelli@yahoo.com>
2015-10-14 15:17:52 +0200

    all ready for iterators

---
+++ es6/iterators_es6.js
@@ -0,0 +1,10 @@
+/*
+	GENERATORZ - ECMAScript 6 generators
+	Author: Marco Faustinelli (contacts@faustinelli.net)
+	Web: http://faustinelli.net/
+	     http://faustinelli.wordpress.com/
+	Version: 0.1
+
+	The GNU GPL/2 License - Copyright (c) 2015 Generatorz Project
+*/
+
---
+++ es6/iterators_es6_test.js
@@ -0,0 +1,16 @@
+/*
+	GENERATORZ - ECMAScript 6 generators
+	Author: Marco Faustinelli (contacts@faustinelli.net)
+	Web: http://faustinelli.net/
+	     http://faustinelli.wordpress.com/
+
+	The GNU GPL/2 License - Copyright (c) 2015 Generatorz Project
+*/
+
+var expect = chai.expect;
+mocha.setup('bdd');
+
+describe('xxx', function() {
+  it('yyy', function() {
+  });
+});
