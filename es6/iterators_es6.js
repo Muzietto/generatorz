6005ed5297d1922cd1415ea29c70d8fd75b4745e
Muzietto <marco_faustinelli@yahoo.com>
2015-10-15 14:50:10 +0200

    a bit of warnings in the text

--- es6/iterators_es6.js
+++ es6/iterators_es6.js
@@ -12,14 +12,54 @@ function take(n, iterable) {
   var count = n;
   var iterator = iterable[Symbol.iterator]();
   return {
+    [Symbol.iterator]() { return this; }, // redundant
     next() {
       if (count > 0) {
         count--;
         return iterator.next();
       } else {
+        //iterator.return(); // not implemented (yet?)
         return { value : undefined, done : true };
-        // close iterator
       }
     }
   }
+}
+
+function zip(iterables) {
+  var iterators = iterables.map(function(it) { return it[Symbol.iterator](); });
+  var running = true;  
+  return {
+    [Symbol.iterator]() { return this; },
+    next() {
+      var result = (running)?{value:[],done:false}:{value:undefined,done:true};
+      /* if (running) {
+       *   var items = iterators.map(iter => iter.next());
+       *   running = items.some(item => item.done);
+       *   if (running) return {value : items.map(item => item.value)}
+       * }
+       * return {done : true}
+       */
+      if (running) {
+        iterators.forEach(function(it) {
+          var next = it.next();
+          if (running && !next.done) {
+            result.value.push(next.value);
+          } else {
+            running = false;
+            result.value = undefined;
+            result.done = true;
+          }
+        });
+      }
+      return result;
+    }
+  };
+}
+
+function naturalNumbers() {
+  var i = 0;
+  return {
+    [Symbol.iterator]() { return this; },
+    next() { return { value : i++, done : false }; }
+  };
 }
--- es6/iterators_es6_test.js
+++ es6/iterators_es6_test.js
@@ -10,10 +10,13 @@
 var expect = chai.expect;
 mocha.setup('bdd');
 
-function expect_from_iterator(iter, value, done) {
-  var data = iter.next();
-  expect(data.value).to.be.eql(value);  
-  expect(data.done).to.be.eql(done);  
+function expect_from_iterator(iter, value, done, times) {
+  times = times || 1;
+  for (var i=0; i<times; i++) {
+    var data = iter.next();
+    expect(data.value).to.be.eql(value);  
+    expect(data.done).to.be.eql(done);
+  }  
 }
 
 describe('among the existing iterators', function() {
@@ -62,16 +65,29 @@ describe('among the existing iterators', function() {
     expect_from_iterator(set_iterator, 'b', false);
     expect_from_iterator(set_iterator, undefined, true);
   });
-  it('an arguments iterator consumes arguments according to the function invocation (not on FF!)', function() {
-    var application = function() {
-      var app_iterator = arguments[Symbol.iterator]();
+  describe('an iterator over arguments', function() {
+    it('handles arguments according to the function invocation (not on FF!)', function() {
+      function application() {
+        return arguments[Symbol.iterator]();
+      };
+      var app_iterator = application('a', 1, true, []);
       expect_from_iterator(app_iterator, 'a', false);
       expect_from_iterator(app_iterator, 1, false);
       expect_from_iterator(app_iterator, true, false);
       expect_from_iterator(app_iterator, [], false);
       expect_from_iterator(app_iterator, undefined, true);      
-    };
-    application('a', 1, true, []);
+    });
+    it('may be based upon the spread operator (not on Chrome!)', function() {
+      function application(...args) {
+        return [...args][Symbol.iterator]();
+      };
+      var app_iterator = application('a', 1, true, []);
+      expect_from_iterator(app_iterator, 'a', false);
+      expect_from_iterator(app_iterator, 1, false);
+      expect_from_iterator(app_iterator, true, false);
+      expect_from_iterator(app_iterator, [], false);
+      expect_from_iterator(app_iterator, undefined, true);      
+    });
   });
   it('an iterator can be built from scratch without any real iterable', function() {
     var iterable = {
@@ -83,6 +99,7 @@ describe('among the existing iterators', function() {
             if (count < 5) {
               return { value : 'pippo', done : false };
             } else {
+              //iterator.return(); // not implemented (yet?)
               return { value : undefined, done : true };
             }
           }
@@ -91,35 +108,84 @@ describe('among the existing iterators', function() {
       }
     }
     var homemade_iterator = iterable[Symbol.iterator]();
-    expect_from_iterator(homemade_iterator, 'pippo', false); // 0
-    expect_from_iterator(homemade_iterator, 'pippo', false); // 1
-    expect_from_iterator(homemade_iterator, 'pippo', false); // 2
-    expect_from_iterator(homemade_iterator, 'pippo', false); // 3
-    expect_from_iterator(homemade_iterator, 'pippo', false); // 4
+    expect_from_iterator(homemade_iterator, 'pippo', false, 5); // 0...4
     expect_from_iterator(homemade_iterator, undefined, true); // 5
+  });
+  describe('an infinite iterator', function() {
+    it.skip('can be used to instantiate variables (not on Chrome!!)', function() {
+      // var [a,b,c] unsupported on Chrome!!!
+      //var [a,b,c] = naturalNumbers(); // extracting next.value
+      expect(a).to.be.eql(0);
+      expect(b).to.be.eql(1);
+      expect(c).to.be.eql(2);
+      expect(naturalNumbers().next().value).to.be.equal(0);
+      //var [d,e] = naturalNumbers();
+      expect(d).to.be.eql(0);
+      expect(e).to.be.eql(1);
+    });
+    it('can be tempered by take', function() {
+      var taker = take(2, naturalNumbers());
+      expect_from_iterator(taker, 0, false);
+      expect_from_iterator(taker, 1, false);
+      expect_from_iterator(taker, undefined, true);
+    });
   });
 });
 
 describe('around iterators men can', function() {
-  it('build a take function', function() {
-    var arr = ['a','b','c','d','e','f'];
-    var taker = take(3, arr);
-    expect_from_iterator(taker, 'a', false);
-    expect_from_iterator(taker, 'b', false);
-    expect_from_iterator(taker, 'c', false);
-    expect_from_iterator(taker, undefined, true);
+  describe('build a take function', function() {
+    it('that extracts a subset', function() {
+      var arr = ['a','b','c','d','e','f'];
+      var taker = take(3, arr);
+      expect_from_iterator(taker, 'a', false);
+      expect_from_iterator(taker, 'b', false);
+      expect_from_iterator(taker, 'c', false);
+      expect_from_iterator(taker, undefined, true);
+      expect_from_iterator(taker, undefined, true);
+    });
+    it('that doesn\'t crash when it overflows', function() {
+      var arr = ['a','b','c'];
+      var taker = take(5, arr);
+      expect_from_iterator(taker, 'a', false);
+      expect_from_iterator(taker, 'b', false);
+      expect_from_iterator(taker, 'c', false);
+      expect_from_iterator(taker, undefined, true);
+      expect_from_iterator(taker, undefined, true);
+      expect_from_iterator(taker, undefined, true);
+    });
   });
-  it('build a zip function', function() {
-    var arr = ['a'];
-    var arr_iterator = arr[Symbol.iterator]();
-    expect_from_iterator(arr_iterator, 'a', false);
-    expect_from_iterator(arr_iterator, undefined, true);
-  });
-  it('', function() {
-    var arr = ['a'];
-    var arr_iterator = arr[Symbol.iterator]();
-    expect_from_iterator(arr_iterator, 'a', false);
-    expect_from_iterator(arr_iterator, undefined, true);
+  describe('build a zip function', function() {
+    it('that zips together n iterables', function() {
+      var arrs = [['a','b','c'],[1,2,3],[null,true,false]];
+      var zipper = zip(arrs);
+      expect_from_iterator(zipper, ['a',1,null], false);
+      expect_from_iterator(zipper, ['b',2,true], false);
+      expect_from_iterator(zipper, ['c',3,false], false);
+      expect_from_iterator(zipper, undefined, true);
+      expect_from_iterator(zipper, undefined, true);
+    });
+    it('respects the shortest iterator', function() {
+      var arrs = [['a','b','c'],[1,2],[null]];
+      var zipper = zip(arrs);
+      expect_from_iterator(zipper, ['a',1,null], false);
+      expect_from_iterator(zipper, undefined, true);
+      expect_from_iterator(zipper, undefined, true);
+    });
+    it('allows mixing different sorts of iterables', function() {
+      var iterable = {
+        [Symbol.iterator]() {
+          var c = -1;
+          return {next() {c++; return (c<25)?{value:c}:{done:true}}}
+        }
+      }
+      var iterables = [['a','b','c'],iterable,'pippo'];
+      var zipper = zip(iterables);
+      expect_from_iterator(zipper, ['a',0,'p'], false);
+      expect_from_iterator(zipper, ['b',1,'i'], false);
+      expect_from_iterator(zipper, ['c',2,'p'], false);
+      expect_from_iterator(zipper, undefined, true);
+      expect_from_iterator(zipper, undefined, true);
+    });
   });
   it('', function() {
     var arr = ['a'];
